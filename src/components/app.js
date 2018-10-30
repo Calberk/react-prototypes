091d20493a3d9efdc53dd4c37e8c9df939e8e593
Calberk <park.edmund@gmail.com>
2018-10-30 09:40:05 -0700

    Completed Functional Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table';
+
+export default () => {
+    return (<div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data={students}/>
+    </div>
+    )
+}
+
+const students = [
+    {
+        name: "Bruce",
+        course: "Running in the Night",
+        grade: 99.4
+    },
+    {
+        name: "Kal-el",
+        course: "Nice alien",
+        grade: 94.6
+    },
+    {
+        name: "Cyborg",
+        course: "Computer Science",
+        grade: 99.9
+    }
+];
 
 
---
+++ src/components/table.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default (props) => {
+    const tableRows = props.data.map(function (item, index){
+        return (<tr key={index}>
+            <td>{item.name}</td>
+            <td>{item.course}</td>
+            <td>{item.grade}</td>
+        </tr>)
+    });
+    return (<table className = "table" >
+        <thead className = "thead-inverse">
+            <tr>
+                <th>Name</th>
+                <th>Course</th>
+                <th>Grade</th>
+            </tr>
+        </thead>
+        <tbody>
+        {tableRows}
+        </tbody>
+    </table>)
+}
