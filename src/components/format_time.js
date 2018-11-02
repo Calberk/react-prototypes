ebf69d338210f841b1375f936fe8071e56544817
Calberk <park.edmund@gmail.com>
2018-11-02 11:02:23 -0700

    stopwatch function working

---
+++ src/components/format_time.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+
+class FormatTime extends Component{
+    constructor(props){
+        super(props);
+        this.timerStyle = {
+            display: 'inline-block',
+            position: 'relative',
+            left: '48%',
+            transform: 'translate(-50%)',
+            width: '295px',
+        };
+
+    this.state = {
+        hour: 0,
+        min: 0,
+        sec: 0,
+        ms: 0
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+         const {elapsed} = nextProps;
+
+         const sec = elapsed/1000;
+         const min = sec/60;
+
+        this.setState({
+            hour: Math.floor(min/60),
+            min: Math.floor(min%60),
+            sec: Math.floor(sec%60),
+            ms: Math.floor(elapsed%100)
+        })
+    }
+
+    leadingZero(number){
+        if(number<10){
+            return number + "0";
+        }
+        return number;
+    }
+
+    render(){
+        const{hour, min, sec, ms} = this.state;
+        return (
+            <div style={this.timerStyle}>{this.leadingZero(hour)}:{this.leadingZero(min)}:{this.leadingZero(sec)}.{this.leadingZero(ms)}</div>
+        )
+    }
+}
+
+export default FormatTime;
