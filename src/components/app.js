d94cbd455e0bf1f5c7e32a6425e2ededd13b4f43
Calberk <park.edmund@gmail.com>
2018-11-03 23:43:02 -0700

    form data inputs are created and saving information

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,19 @@
+import React from 'react';
+import ContactList from './contact-list';
+import ContactForm from './contact_form';
 
+export default () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Address Book</h1>
+            <div className="row">
+                <div className="col-4">
+                    <ContactForm/>
+                </div>
+                <ContactList/>
+            </div>
+        </div>
+    )
+
+};
 
---
+++ src/data/contacts.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        firstName: 'Edmund',
+        lastName: 'Park',
+        phone: '7142874000',
+        email: 'park.edmund@learningfuze.com'
+    },
+    {
+        firstName: 'Andrew',
+        lastName: 'Oatu',
+        phone: '7142443000',
+        email: 'AndrewOatu@learningfuze.com'
+    },
+    {
+        firstName: 'Shridhar',
+        lastName: 'Madala',
+        phone: '7143243000',
+        email: 'ShriM@learningfuze.com'
+    },
+    {
+        firstName: 'Kyle',
+        lastName: 'Umbarger',
+        phone: '5623243000',
+        email: 'ChronicSufr@learningfuze.com'
+    },
+    {
+        firstName: 'TIffany',
+        lastName: 'Kim',
+        phone: '5652348000',
+        email: 'StiffTiff@learningfuze.com'
+    },
+]
