23a50cbec6158aaabc2df021e05eb6865072f7f4
Calberk <park.edmund@gmail.com>
2018-11-19 13:26:36 -0800

    completed redux clock

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React from 'react';
+import Clock from './clock';
 
 
+export default ()=>{
+    return(
+        <div className="container">
+            <h1 className="text-center">This is the beginning of the end</h1>
+            <Clock/>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers/index';
+import { createStore } from 'redux';
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <Provider store={createStore(rootReducer)}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
